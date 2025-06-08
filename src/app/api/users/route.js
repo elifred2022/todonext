1428b9ed8afeef0eefa8817351b1608abcb16be3
elifred2022 <elifed21@hotmail.com>
2025-06-08 18:39:06 -0300

    hecho lo mas basico, desde aca haremos una aplciacion crud

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,12 +1,24 @@
 import { NextResponse } from "next/server";
 
+console.log(process.env.TOKEN);
+console.log(process.env.SECRET_KEY);
+
 export async function GET() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/users?token=" +
+      process.env.NEXT_PUBLIC_TOKEN
+  );
+  console.log(
+    "https://jsonplaceholder.typicode.com/users?token=" +
+      process.env.NEXT_PUBLIC_TOKEN
+  );
   const data = await res.json();
   return NextResponse.json(data);
 }
 
-export function POST() {
+export async function POST(request) {
+  const { nombre, apellido } = await request.json();
+  console.log(nombre, apellido);
   return NextResponse.json({
     massage: "publicando",
   });
